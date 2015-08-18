32e98d76016ba5e35513c09cdfe48e36d99a5986
wanshanhu79 <994413754@qq.com>
2015-08-18 10:55:40 +0800

    navigaiton修改

---
+++ TalentService/TalentService/Classes/Main(主要)/Controller/ZZNaviController.h
@@ -0,0 +1,13 @@
+//
+//  ZZNaviController.h
+//  TalentService
+//
+//  Created by zhizhen on 15/8/18.
+//  Copyright (c) 2015年 zhizhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZZNaviController : UINavigationController
+
+@end
---
+++ TalentService/TalentService/Classes/Main(主要)/Controller/ZZNaviController.m
@@ -0,0 +1,93 @@
+//
+//  ZZNaviController.m
+//  TalentService
+//
+//  Created by zhizhen on 15/8/18.
+//  Copyright (c) 2015年 zhizhen. All rights reserved.
+//
+
+#import "ZZNaviController.h"
+
+@interface ZZNaviController ()<UINavigationControllerDelegate>
+@property (nonatomic, strong) id popDelegate;
+@end
+
+@implementation ZZNaviController
+
+
++ (void)initialize
+{
+    // 设置整个项目所有item的主题样式
+    UIBarButtonItem *item = [UIBarButtonItem appearance];
+    
+    // 设置普通状态
+    // key：NS****AttributeName
+    NSMutableDictionary *textAttrs = [NSMutableDictionary dictionary];
+    textAttrs[NSForegroundColorAttributeName] = [UIColor orangeColor];
+    textAttrs[NSFontAttributeName] = [UIFont systemFontOfSize:15];
+    [item setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
+    
+    // 设置不可用状态
+    NSMutableDictionary *disableTextAttrs = [NSMutableDictionary dictionary];
+    disableTextAttrs[NSForegroundColorAttributeName] = [UIColor colorWithRed:0.6 green:0.6 blue:0.6 alpha:0.7];
+    disableTextAttrs[NSFontAttributeName] = textAttrs[NSFontAttributeName];
+    [item setTitleTextAttributes:disableTextAttrs forState:UIControlStateDisabled];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    _popDelegate = self.interactivePopGestureRecognizer.delegate;
+    self.delegate = self;
+}
+
+/**
+ *  重写这个方法目的：能够拦截所有push进来的控制器
+ *
+ *  @param viewController 即将push进来的控制器
+ */
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+    if (self.childViewControllers.count) { // 这时push进来的控制器viewController，不是第一个子控制器（不是根控制器）
+        /* 自动显示和隐藏tabbar */
+        viewController.hidesBottomBarWhenPushed = YES;
+        
+        /* 设置导航栏上面的内容 */
+        // 设置左边的返回按钮
+        viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"返回" style:UIBarButtonItemStylePlain target:self action:@selector(back)];
+   
+    }
+    
+    [super pushViewController:viewController animated:animated];
+}
+
+- (void)back
+{
+#warning 这里要用self，不是self.navigationController
+    // 因为self本来就是一个导航控制器，self.navigationController这里是nil的
+    [self popViewControllerAnimated:YES];
+}
+
+- (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+    if (self.childViewControllers.count == 0) { // 是根控制器
+        self.interactivePopGestureRecognizer.delegate = _popDelegate;
+        
+        
+    }else{ // 非根控制器
+        self.interactivePopGestureRecognizer.delegate = nil;
+        
+    }
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
