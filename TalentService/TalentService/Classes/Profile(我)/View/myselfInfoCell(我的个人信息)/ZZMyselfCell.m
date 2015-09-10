2a4c4c3c6c170f43c9becce70faca1cb2cfbeda2
CharlesWl <835963611@qq.com>
2015-09-10 20:47:41 +0800

    Revert "Revert "Revert "fsdfas"""
    This reverts commit 2ecbd604168a3e02ac4fb465e85ab319a5de0475.

--- TalentService/TalentService/Classes/Profile(我)/View/myselfInfoCell(我的个人信息)/ZZMyselfCell.m
+++ TalentService/TalentService/Classes/Profile(我)/View/myselfInfoCell(我的个人信息)/ZZMyselfCell.m
@@ -10,5 +10,14 @@
 
 @implementation ZZMyselfCell
 
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
 
 @end
